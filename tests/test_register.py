1d74cdd63dc71b1cb6896fcca7c7acc1fac7b8e7
David Monts <david.monts@sonicautomotive.com>
2022-05-01 22:01:05 -0400

    Adding testing for registering systems and alerts.

--- tests/test_register.py
+++ tests/test_register.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import pytest
 import logging
 import os
+import json
 
 from main import app
 from alert_db import get_db
@@ -48,6 +49,53 @@ def setup_teardown_db():
 def test_register_system():
     
     response = client.post("/register/system", 
-                           json={"system_nm": "anothpricing1", 
+                           json={"system_nm": "anothpricing2", 
                                  "system_desc": "The pricing system is responsible for accurately pricing vehicles in a responsive manner"})
     assert response.status_code == 200
+    res = json.loads(response.text)
+    assert res["system_nm"] == "anothpricing2"
+    assert res["system_desc"] == "The pricing system is responsible for accurately pricing vehicles in a responsive manner"
+    assert res["system_id"] == 2
+    
+    response = client.post("/register/system", 
+                           json={"system_nm": "anothpricing0", 
+                                 "system_desc": "The pricing system is responsible for accurately pricing vehicles in a responsive manner"})
+    assert response.status_code == 200
+    res = json.loads(response.text)
+    assert res["system_nm"] == "anothpricing0"
+    assert res["system_desc"] == "The pricing system is responsible for accurately pricing vehicles in a responsive manner"
+    assert res["system_id"] == 3
+    
+
+def test_register_diag():
+    response = client.post("/register/alert", 
+                           json={"alert_nm": "firstalert", 
+                                 "alert_desc": "Ensure the world is working"})
+    assert response.status_code == 200
+    alert_res = json.loads(response.text)
+    assert alert_res["alert_nm"] == "firstalert"
+    assert alert_res["alert_desc"] == "Ensure the world is working"
+    assert alert_res["alert_id"] == 2
+    
+    response = client.post("/register/alert", 
+                           json={"alert_nm": "firstalert", 
+                                 "alert_desc": "Ensure the world is working"})
+    assert response.status_code == 400
+    
+    response = client.post("/register/alert", 
+                           json={"alert_nm": "2ndalert", 
+                                 "system_nm": "anothpricing2",
+                                 "alert_desc": "Ensure the world is working still"})
+    assert response.status_code == 200
+    alert_res = json.loads(response.text)
+    assert alert_res["alert_nm"] == "2ndalert"
+    assert alert_res["alert_desc"] == "Ensure the world is working still"
+    assert alert_res["alert_id"] == 3
+    
+    response = client.post("/register/alert", 
+                           json={"alert_nm": "3rdalert", 
+                                 "system_nm": "anothpricing15",
+                                 "alert_desc": "Ensure the world is working still"})
+    assert response.status_code == 400
+    
+    
